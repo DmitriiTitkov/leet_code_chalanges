e3a2bb37cd77cf99fda62763a5c077e6128f3666
Dmitrii Titkov <dumacukps@gmail.com>
2022-02-18 23:46:10 +0000

    Binary search for find min in rotated sorted array

---
+++ medium/find_minimum_in_rotated_sorted_array.py
@@ -0,0 +1,98 @@
+"""
+Suppose an array of length n sorted in ascending order is rotated between 1 and
+n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
+[4,5,6,7,0,1,2] if it was rotated 4 times.
+[0,1,2,4,5,6,7] if it was rotated 7 times.
+Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results
+in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+
+Given the sorted rotated array nums of unique elements, return the minimum
+element of this array.
+You must write an algorithm that runs in O(log n) time.
+
+Example 1:
+Input: nums = [3,4,5,1,2]
+Output: 1
+Explanation: The original array was [1,2,3,4,5] rotated 3 times.
+
+Example 2:
+Input: nums = [4,5,6,7,0,1,2]
+Output: 0
+Explanation: The original array was [0,1,2,4,5,6,7] and it was rotated 4 times.
+
+Example 3:
+Input: nums = [11,13,15,17]
+Output: 11
+Explanation: The original array was [11,13,15,17] and it was rotated 4 times.
+
+Constraints:
+n == nums.length
+1 <= n <= 5000
+-5000 <= nums[i] <= 5000
+All the integers of nums are unique.
+nums is sorted and rotated between 1 and n times.
+"""
+from typing import List
+
+import pytest
+
+
+class Solution:
+    """
+    Time: O(logn)
+    Space: O(1)
+    Simple solution using first element as min benchmark
+    """
+    def findMin(self, nums: List[int]) -> int:
+        l = 0
+        r = len(nums) - 1
+
+        curr_min = nums[0]
+
+        while l <= r:
+            mid = (l + r) // 2
+
+            if nums[mid] >= curr_min:
+                l = mid + 1
+            else:
+                curr_min = nums[mid]
+                r = mid - 1
+
+        return curr_min
+
+
+class Solution2:
+    """
+    Time: O(logn)
+    Space: O(1)
+    Use right elemt to check whether there are smaller nums on the right
+    """
+    def findMin(self, nums: List[int]) -> int:
+        l = 0
+        r = len(nums) - 1
+
+        while l < r:
+            mid = (l + r) // 2
+
+            if nums[mid] > nums[r]:
+                l = mid + 1
+            else:
+                r = mid
+
+        return nums[l]
+
+
+@pytest.mark.parametrize(
+    "solution", [Solution, Solution2]
+)
+@pytest.mark.parametrize(
+    "nums,expected_result", (
+        ([3, 4, 5, 1, 2], 1),
+        ([4, 5, 6, 7, 0, 1, 2], 0),
+        ([11, 13, 15, 17], 11),
+        ([2, 1], 1),
+        ([3,1,2], 1),
+    )
+)
+def test_search(solution, nums, expected_result):
+    assert solution().findMin(nums) == expected_result
