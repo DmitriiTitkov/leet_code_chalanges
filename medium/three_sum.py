20bbe90b841664fde3703a2c6e501e38cc81d06b
Dmitrii Titkov <dumacukps@gmail.com>
2022-02-28 23:28:50 +0000

    Threesum n**2 solution

--- medium/three_sum.py
+++ medium/three_sum.py
@@ -25,26 +25,40 @@ import pytest
 
 
 class Solution:
+    """
+    A bit strange task. Had some difficulties removing the duplicates from
+    final output.
+
+    Time: O(n**2)
+    Space: O(1)
+    """
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) < 3:
+            return []
+
         nums.sort()
         res = []
         i = 0
 
-        while nums[i] < 0:
-            l = i + 1
-            r = i + 2
+        while i < len(nums) and nums[i] <= 0:
+            while 0 < i < len(nums) and nums[i] == nums[i-1]:
+                i += 1
 
+            l = i+1
+            r = len(nums) - 1
             while l < r:
-                op_num = - nums[i]
-                if nums[l] + nums[r] == op_num:
-                    res.append([nums[i], nums[l], nums[r]])
-                    break
+                sum_num = nums[i] + nums[l] + nums[r]
 
-                elif r + 1 <= len(nums) - 1 and nums[l] + nums[r+1] <= op_num:
-                    r+=1
+                if sum_num > 0:
+                    r -= 1
+                elif sum_num < 0:
+                    l += 1
                 else:
-                    l+=1
-
+                    res.append([nums[i], nums[l], nums[r]])
+                    r -= 1
+                    l += 1
+                    while 0 <= r <= len(nums) - 1 and nums[r] == nums[r + 1]:
+                        r -= 1
             i += 1
 
         return res
@@ -56,6 +70,9 @@ class Solution:
 @pytest.mark.parametrize(
     "nums,expected_result", (
             ([-1, 0, 1, 2, -1, -4], [[-1, -1, 2], [-1, 0, 1]]),
+            ([0, 0, 0], [[0, 0, 0]]),
+            ([0, 0, 0, 0], [[0, 0, 0]]),
+            ([-2, 0, 0, 2, 2], [[-2, 0, 2]]),
     )
 )
 def test_search(nums, expected_result):
