e5be3f1565abadfd3a9e8ced16813b72769e0540
Dmitrii Titkov <dumacukps@gmail.com>
2022-04-17 10:13:23 +0100

    working DP first try

---
+++ medium/jump_game_ii.py
@@ -0,0 +1,57 @@
+"""
+Given an array of non-negative integers nums, you are initially positioned at
+the first index of the array. Each element in the array represents your maximum
+jump length at that position. Your goal is to reach the last index in the
+minimum number of jumps. You can assume that you can always reach the last
+index.
+
+Example 1:
+Input: nums = [2,3,1,1,4]
+Output: 2
+Explanation: The minimum number of jumps to reach the last index is 2. Jump 1
+step from index 0 to 1, then 3 steps to the last index.
+
+Example 2:
+Input: nums = [2,3,0,1,4]
+Output: 2
+
+Constraints:
+1 <= nums.length <= 104
+0 <= nums[i] <= 1000
+"""
+from typing import List
+
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        # 2, 3, 1, 1, 4
+        #    1
+        # .      2|
+        if len(nums) == 1:
+            return 0
+
+        total_jumps = 1
+
+        next_max_jump = 0
+        cur_jump = nums[0]
+        i = 0
+
+        while i < len(nums ) -1:
+            for j in range(i, i+ cur_jump + 1):
+                next_max_jump -= 1
+                if j == len(nums) - 1:
+                    break
+                next_max_jump = max((
+                    next_max_jump,
+                    nums[j]
+                ))
+
+            i = j
+
+            if i == len(nums) - 1:
+                break
+            total_jumps += 1
+            cur_jump = next_max_jump
+
+        return total_jumps
+
