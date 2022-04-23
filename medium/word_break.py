c5866c7b5a5f542335484fe55c6b364918d7fb54
Dmitrii Titkov <dumacukps@gmail.com>
2022-04-23 21:15:35 +0100

    DP Solution

--- medium/word_break.py
+++ medium/word_break.py
@@ -35,18 +35,30 @@ import pytest
 class Solution:
     """DFS Solution
     Time: O(n) - as the word limited to 20 characters and doesn't grow with
-          input.
-    Space: O(n) - worst case complexity for memorization
+          input. O(n) is very arguable as limiting factor doesn't work for short
+          strings, and the constant is still quite big.
+
+          Considering that min(lenght, 20) works as a constant:
+          m + n * 20 * 20
+
+          Considering lsn(s) < 20:
+          m + n * n * n
+
+    Space: O(n+m) - worst case complexity for memorization abd set
     """
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        # o(m) - m - length of wordDict
         w_dict = set(wordDict)
 
         @lru_cache
         def dfs(start: int) -> bool:
             res = False
+            # loop of max 20
             for end in range(start, min(len(s) + 1, start + 20)):
+                # slice of max 20
                 substr = s[start:end]
 
+                # o(1) inclusion check
                 if substr in w_dict:
                     if end == len(s):
                         return True
@@ -59,6 +71,37 @@ class Solution:
         return dfs(0)
 
 
+class Solution2:
+    """
+    DP Solution
+    Time complexity is similar to DFS solution and depends on weather to accept
+    min(20,n) as a constant.
+
+    Time: O(m + n + n * min(20,n) * min(20,n))
+    Space: O(n+m)
+    """
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        # n
+        dp = [False] * (len(s) + 1)
+        dp[-1] = True
+        # m
+        word_set = set(wordDict)
+
+        # n
+        for start in range(len(s), -1, -1):
+            # min(20, n)
+            for stop in range(start, min(len(s), start+20)):
+                # slice min(20, n)
+                if s[start:stop+1] in word_set and dp[stop+1]:
+                    dp[start] = True
+                    break
+
+        return dp[0]
+
+
+@pytest.mark.parametrize(
+    "solution", [Solution, Solution2]
+)
 @pytest.mark.parametrize(
     "s,word_dict,expected_result",
     (
@@ -67,5 +110,5 @@ class Solution:
         ("catsandog", ["cats", "dog", "sand", "and", "cat"], False),
     )
 )
-def test_word_break(s, word_dict, expected_result):
+def test_word_break(solution, s, word_dict, expected_result):
     assert Solution().wordBreak(s, word_dict) == expected_result
