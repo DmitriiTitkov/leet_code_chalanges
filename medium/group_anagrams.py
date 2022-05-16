3427b27f3b625ff568e41a715b62d33167bd85d2
Dmitrii Titkov <dumacukps@gmail.com>
2022-05-16 22:26:57 +0100

    Use char counts as key

--- medium/group_anagrams.py
+++ medium/group_anagrams.py
@@ -32,7 +32,7 @@ class Solution:
     """Use sorted word as for gouping
 
     Time: O(n * k logk)
-    Space: O(n)
+    Space: O(n * k)
     """
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         anagrams = defaultdict(list)
@@ -42,6 +42,29 @@ class Solution:
             anagrams[sorted_word].append(word)
 
         return [v for v in anagrams.values()]
+
+
+class Solution2:
+    """
+    Use character count for grouping
+    Time: O(n * k)
+    Space: O (n * k)
+    """
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = defaultdict(list)
+
+        for word in strs:
+            char_count = [0] * 26
+            shift = ord('a')
+
+            for char in word:
+                char_count[ord(char) - shift] += 1
+
+            char_count = tuple(char_count)
+            anagrams[char_count].append(word)
+
+        return [v for v in anagrams.values()]
+
 
 
 @pytest.mark.parametrize(
