92fa7eb2982f992e9c6bcd1cd59669cea964ea0c
Dmitrii Titkov <dumacukps@gmail.com>
2022-06-05 17:09:59 +0100

    Use extra space to find greater and lesser numbers

---
+++ medium/increasing_triplet_subsequence.py
@@ -0,0 +1,74 @@
+"""
+Given an integer array nums, return true if there exists a triple of indices
+(i, j, k) such that i < j < k and nums[i] < nums[j] < nums[k]. If no such
+indices exists, return false.
+
+Example 1:
+Input: nums = [1,2,3,4,5]
+Output: true
+Explanation: Any triplet where i < j < k is valid.
+
+Example 2:
+Input: nums = [5,4,3,2,1]
+Output: false
+Explanation: No triplet exists.
+
+Example 3:
+Input: nums = [2,1,5,0,4,6]
+Output: true
+Explanation: The triplet (3, 4, 5) is valid because
+nums[3] == 0 < nums[4] == 4 < nums[5] == 6.
+
+Constraints:
+1 <= nums.length <= 5 * 105
+-231 <= nums[i] <= 231 - 1
+
+
+Follow up: Could you implement a solution that runs in O(n) time complexity and
+O(1) space complexity?
+"""
+from typing import List
+
+import pytest
+
+
+class Solution:
+    """Use additional arrays to keep record of max on the right and min on the
+    left.
+    Time: O(n)
+    Space: O(n)
+    """
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        length = len(nums)
+        has_smaller_num = [False] * length
+        has_greater_num = [False] * length
+
+        min_num = float("inf")
+        for i in range(length):
+            min_num = min(min_num, nums[i])
+            if nums[i] > min_num:
+                has_smaller_num[i] = True
+
+        max_num = float("-inf")
+        for i in range(length - 1, -1, -1):
+            max_num = max(max_num, nums[i])
+            if nums[i] < max_num:
+                has_greater_num[i] = True
+
+        for i in range(length):
+            if has_smaller_num[i] and has_greater_num[i]:
+                return True
+
+        return False
+
+
+@pytest.mark.parametrize(
+    "nums,expected_output", (
+        ([1, 2, 3, 4, 5], True),
+        ([5, 4, 3, 2, 1], False),
+        ([2, 1, 5, 0, 4, 6], True),
+
+    )
+)
+def test_increasing_triplet(nums, expected_output):
+    assert Solution().increasingTriplet(nums) == expected_output
