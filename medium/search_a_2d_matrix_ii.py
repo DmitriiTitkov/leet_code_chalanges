b37d67efde6c0aea3a67731a39bb95f03c311394
Dmitrii Titkov <dumacukps@gmail.com>
2022-07-24 10:08:19 +0100

    Binary search

---
+++ medium/search_a_2d_matrix_ii.py
@@ -0,0 +1,86 @@
+"""
+Write an efficient algorithm that searches for a value target in an m x n
+integer matrix matrix. This matrix has the following properties:
+Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
+
+Example 1:
+Input: matrix =
+[[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]],
+target = 5
+Output: true
+
+Example 2:
+Input: matrix =
+[[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]],
+target = 20
+Output: false
+
+
+Constraints:
+m == matrix.length
+n == matrix[i].length
+1 <= n, m <= 300
+-109 <= matrix[i][j] <= 109
+All the integers in each row are sorted in ascending order.
+All the integers in each column are sorted in ascending order.
+-109 <= target <= 109
+"""
+from typing import List
+
+import pytest
+
+
+class Solution:
+    """
+    Binary search solution.
+    Time: O(n * logk)
+    Space: O(1)
+    """
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        row_count = len(matrix)
+        col_count = len(matrix[0])
+
+        for row in range(row_count):
+            l, r = 0, col_count - 1
+
+            while l <= r:
+                mid = (l + r) // 2
+                if matrix[row][mid] == target:
+                    return True
+                elif matrix[row][mid] < target:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+
+        return False
+
+
+@pytest.mark.parametrize(
+    "matrix,target,expected_result", (
+    (
+        [
+            [1, 4, 7, 11, 15],
+            [2, 5, 8, 12, 19],
+            [3, 6, 9, 16, 22],
+            [10, 13, 14, 17, 24],
+            [18, 21, 23, 26, 30]
+        ],
+        5,
+        True
+    ),
+    (
+        [
+            [1, 4, 7, 11, 15],
+            [2, 5, 8, 12, 19],
+            [3, 6, 9, 16, 22],
+            [10, 13, 14, 17, 24],
+            [18, 21, 23, 26, 30]
+        ],
+        20,
+        False
+    )
+)
+)
+def test_search_matrix(matrix, target, expected_result):
+    assert Solution().searchMatrix(matrix, target) == expected_result
