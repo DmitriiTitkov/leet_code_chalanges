011dc5f77f38851c0c1bcad30a498ee86f0f2463
Dmitrii Titkov <dumacukps@gmail.com>
2022-07-25 21:44:37 +0100

    Simplify binary search leaving only single search method

--- medium/find_first_and_last_position_of_element_in_sorted_array.py
+++ medium/find_first_and_last_position_of_element_in_sorted_array.py
@@ -28,57 +28,46 @@ from typing import List
 
 import pytest
 
+
 class Solution:
-    """First iteration:
-    Time: O(log n)
-    Space: O(log n) - for stack
     """
+    Binary search to find leftmost index for target and leftmost index for
+    target+1.
+    Time: O(logn)
+    Space: O(1)
+
+    n - length of nums
+    """
+    def bin_search(self, nums, target) -> int:
+        l, r = 0, len(nums) - 1
+        while l < r:
+            mid = (l + r) // 2
+            if nums[mid] >= target:
+                r = mid
+            else:
+                l = mid + 1
+        return l
+
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        if not nums:
+        if len(nums) == 0:
             return [-1, -1]
-        left = self.find_starting(nums, target, 0, len(nums) - 1)
-        right = self.find_ending(nums, target, 0, len(nums) - 1)
+
+        left = self.bin_search(nums, target)
+        if nums[left] != target:
+            return [-1, -1]
+        right = self.bin_search(nums, target + 1)
+        if nums[right] != target:
+            right -= 1
+
         return [left, right]
-
-    def find_starting(self, nums: List[int], target, l: int, r: int) -> int:
-        if l == r:
-            if nums[l] == target:
-                return l
-            else:
-                return -1
-
-        mid = (l+r) // 2
-        if target <= nums[mid]:
-            return self.find_starting(nums, target, l, mid)
-        else:
-            return self.find_starting(nums, target, mid+1, r)
-
-    def find_ending(self, nums: List[int], target: int, l: int, r: int) -> int:
-        if l == r:
-            if nums[l] == target:
-                return l
-            else:
-                return -1
-
-        mid = (l + r) // 2
-        if mid < 0:
-            return -1
-        if target >= nums[mid]:
-            right = self.find_ending(nums, target, mid+1, r)
-            if right == -1 and nums[mid] == target:
-                return mid
-            return right
-        else:
-            if l == mid:
-                return -1
-            return self.find_ending(nums, target, l, mid-1)
 
 
 @pytest.mark.parametrize(
     "nums,target,expected_output",
     (
-        ([5,7,7,8,8,10], 8, [3,4]),
-        ([0,0,1,1,1,2,2,3,3,3,4,4,4,4,5,5,6,6,6,8,10,10], 4, [10, 13]),
+        ([5, 7, 7, 8, 8, 10], 8, [3, 4]),
+        ([5, 7, 7, 8, 10, 10], 10, [4, 5]),
+        ([0, 0, 1, 1, 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 6, 6, 6, 8, 10, 10], 4, [10, 13]),
 
     )
 )
